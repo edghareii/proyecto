0a3f6542c251b0af4a318f2751b4dd306e0621d1
edgardo <edgar_king1@hotmail.com>
2016-12-07 21:10:18 -0800

    Añadir librerias para poder realizar el modo grafico

---
+++ Diagnostico.pl
@@ -0,0 +1,2 @@
+:-use_module(library(pce)).%Libreria para relaizarlo en modo grafico
+:-use_module(library(pce_style_item)). %libreria que permite crear label, button etc para hacer diseño del sistema.
